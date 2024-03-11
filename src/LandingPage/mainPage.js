470be5a867286f55fcf59595745b50c2a13e8ad2
RitikaETAOP <ritika@etaop.in>
2024-03-12 00:32:08 +0530

    README.md Update

--- src/LandingPage/mainPage.js
+++ src/LandingPage/mainPage.js
@@ -1,31 +1,31 @@
 import * as React from 'react';
-import {Box} from "@mui/material";
+import {alpha, Box, Stack, TextField} from "@mui/material";
 import '../App.css';
 import SwipeableViews from 'react-swipeable-views';
 import {autoPlay} from 'react-swipeable-views-utils';
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Button from "@mui/material/Button";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
     {
         label: 'San Francisco – Oakland Bay Bridge, United States',
-        imgPath:
-            'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+        imgPath: './Images/A1.png'
     },
     {
         label: 'Bird',
-        imgPath:
-            'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+        imgPath: 'https://img.freepik.com/free-vector/watercolor-nature-background-with-leaves_52683-59449.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709510400&semt=ais',
     },
     {
         label: 'Bali, Indonesia',
-        imgPath:
-            'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+        imgPath: 'https://static.vecteezy.com/system/resources/thumbnails/022/399/950/original/green-watercolor-background-with-leaves-animation-for-wedding-invitation-free-video.jpg',
     },
     {
         label: 'Goč, Serbia',
-        imgPath:
-            'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+        imgPath: 'https://daadiplanet.in/wp-content/uploads/2024/02/2996978.jpg'
     },
 ];
 
@@ -42,7 +42,7 @@ function Main() {
     return (
 
         <Box>
-            {/*Slider*/}
+            {/* Slider */}
             <AutoPlaySwipeableViews
                 index={activeStep}
                 onChangeIndex={handleStepChange}
@@ -51,21 +51,118 @@ function Main() {
                 {images.map((step, index) => (
                     <div key={step.label}>
                         {Math.abs(activeStep - index) <= 2 ? (
-                            <Box
-                                component="img"
-                                sx={{
-                                    height: '100%',
-                                    display: 'block',
-                                    overflow: 'hidden',
-                                    width: '100%',
-                                }}
-                                src={step.imgPath}
-                                alt={step.label}
-                            />
+                            <Box>
+                                <Box
+                                    component="img"
+                                    sx={{
+                                        height: '100%',
+                                        display: 'block',
+                                        overflow: 'hidden',
+                                        width: '100%',
+                                    }}
+                                    src={step.imgPath}
+                                    alt={step.label}
+                                />
+                            </Box>
+
                         ) : null}
                     </div>
                 ))}
             </AutoPlaySwipeableViews>
+
+            <Container
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    pt: {xs: 14, sm: 20},
+                    pb: {xs: 8, sm: 12},
+                }}
+            >
+                <Stack spacing={2} useFlexGap sx={{width: {xs: '100%', sm: '70%'}}}>
+                    <Typography
+                        component="h1"
+                        variant="h1"
+                        sx={{
+                            display: 'flex',
+                            flexDirection: {xs: 'column', md: 'row'},
+                            alignSelf: 'center',
+                            textAlign: 'center',
+                        }}
+                    >
+                        Our latest&nbsp;
+                        <Typography
+                            component="span"
+                            variant="h1"
+                            sx={{
+                                color: (theme) =>
+                                    theme.palette.mode === 'light' ? 'primary.main' : 'primary.light',
+                            }}
+                        >
+                            products
+                        </Typography>
+                    </Typography>
+                    <Typography variant="body1" textAlign="center" color="text.secondary">
+                        Explore our cutting-edge dashboard, delivering high-quality solutions
+                        tailored to your needs. <br/>
+                        Elevate your experience with top-tier features and services.
+                    </Typography>
+                    <Stack
+                        direction={{xs: 'column', sm: 'row'}}
+                        alignSelf="center"
+                        spacing={1}
+                        useFlexGap
+                        sx={{pt: 2, width: {xs: '100%', sm: 'auto'}}}
+                    >
+                        <TextField
+                            id="outlined-basic"
+                            hiddenLabel
+                            size="small"
+                            variant="outlined"
+                            aria-label="Enter your email address"
+                            placeholder="Your email address"
+                            inputProps={{
+                                autocomplete: 'off',
+                                ariaLabel: 'Enter your email address',
+                            }}
+                        />
+                        <Button variant="contained" color="primary">
+                            Start now
+                        </Button>
+                    </Stack>
+                    <Typography variant="caption" textAlign="center" sx={{opacity: 0.8}}>
+                        By clicking &quot;Start now&quot; you agree to our&nbsp;
+                        <Link href="#" color="primary">
+                            Terms & Conditions
+                        </Link>
+                        .
+                    </Typography>
+                </Stack>
+                <Box
+                    id="image"
+                    sx={(theme) => ({
+                        mt: {xs: 8, sm: 10},
+                        alignSelf: 'center',
+                        height: {xs: 200, sm: 700},
+                        width: '100%',
+                        backgroundImage:
+                            theme.palette.mode === 'light'
+                                ? 'url("/static/images/templates/templates-images/hero-light.png")'
+                                : 'url("/static/images/templates/templates-images/hero-dark.png")',
+                        backgroundSize: 'cover',
+                        borderRadius: '10px',
+                        outline: '1px solid',
+                        outlineColor:
+                            theme.palette.mode === 'light'
+                                ? alpha('#BFCCD9', 0.5)
+                                : alpha('#9CCCFC', 0.1),
+                        boxShadow:
+                            theme.palette.mode === 'light'
+                                ? `0 0 12px 8px ${alpha('#9CCCFC', 0.2)}`
+                                : `0 0 24px 12px ${alpha('#033363', 0.2)}`,
+                    })}
+                />
+            </Container>
 
         </Box>
 
